c893219bd2ccbd8ef2c412217459c999e276d160
elzado001 <153523520+elzado001@users.noreply.github.com>
2025-02-11 18:30:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,16 +11,3 @@ menuCloseIcon .addEventListener('click', function() {
     mobileMenuContainer.classList.remove('active');
 });
 
-
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-    const message = document.getElementById('no-right-click-message');
-    if (message) {
-        message.style.display = 'block';
-        setTimeout(() => {
-            message.style.display = 'none';
-        }, 2000); // Hide the message after 2 seconds
-    }
-});
-
-
